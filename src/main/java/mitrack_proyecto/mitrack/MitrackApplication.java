a1230e0f01fb571b782d32f931e91d3554bb0d20
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-05-19 19:31:22 -0400

    First Commit

---
+++ src/main/java/mitrack_proyecto/mitrack/MitrackApplication.java
@@ -0,0 +1,13 @@
+package mitrack_proyecto.mitrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitrackApplication.class, args);
+	}
+
+}
