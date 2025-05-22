2264113597f0443d2db9ff68687efc6b93c99020
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-05-22 15:36:36 -0400

    Listos model(rol, usuario)

--- src/main/java/mitrack_proyecto/mitrack/model/Rol.java
+++ src/main/java/mitrack_proyecto/mitrack/model/Rol.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
