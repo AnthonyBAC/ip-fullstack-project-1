bb49c065ae2029be199ebc797a1fc5fe824e18d5
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-05-22 14:59:38 -0400

    Empresa controller listo, actualizacion de nombres

--- src/main/java/mitrack_proyecto/mitrack/controller/EmpresaController.java
+++ src/main/java/mitrack_proyecto/mitrack/controller/EmpresaController.java
@@ -9,6 +9,38 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/empresa")
-public class empresaController {
+public class EmpresaController {
+    @Autowired
+    private EmpresaService empresa_service;
+
+    @GetMapping
+    public List<Empresa> obtenerEmpresas() {
+        return empresa_service.obtenerEmpresas();
+    }
+
+    @GetMapping("{id}")
+    public Empresa buscarPorId(@PathVariable int id_emp) {
+        return empresa_service.buscarPorId(id_emp);
+    }
+
+    @GetMapping("/empresa/{nombre}")
+    public Empresa buscarEmpresaPorNombre(@PathVariable("nombre") String nombre) {
+        return empresa_service.buscarPorNombre(nombre);
+    }
+
+    @PostMapping
+    public Empresa guardarEmpresa(@RequestBody Empresa empresa) {
+        return empresa_service.guardarEmpresa(empresa);
+    }
+
+    @DeleteMapping({ "id" })
+    public String elimarEmpresaPorId(@PathVariable int id_emp) {
+        return empresa_service.eliminarEmpresaPorId(id_emp);
+    }
+
+    @PutMapping({ "id" })
+    public Empresa actualizarEmpresa(@PathVariable int id_emp, @RequestBody Empresa empresa) {
+        return empresa_service.actualizarEmpresa(empresa);
+    }
 
 }
--- src/main/java/mitrack_proyecto/mitrack/services/EmpresaService.java
+++ src/main/java/mitrack_proyecto/mitrack/services/EmpresaService.java
@@ -11,32 +11,32 @@ public class EmpresaService {
 
     // Inyeccion de repositorio para utilizar sus metodos
     @Autowired
-    private EmpresaRepository empresaRepository;
+    private EmpresaRepository empresa_repository;
 
     // Llamando metodos desde repository
-    public List<Empresa> getEmpresas() {
-        return empresaRepository.obtener_empresas();
+    public List<Empresa> obtenerEmpresas() {
+        return empresa_repository.obtenerEmpresas();
     }
 
-    public Empresa buscar_por_id(int id_emp) {
-        return empresaRepository.buscar_por_id(id_emp);
+    public Empresa buscarPorId(int id_emp) {
+        return empresa_repository.buscarPorId(id_emp);
     }
 
-    public Empresa buscar_por_nombre(String nombre_emp) {
-        return empresaRepository.buscar_por_nombre(nombre_emp);
+    public Empresa buscarPorNombre(String nombre_emp) {
+        return empresa_repository.buscarPorNombre(nombre_emp);
     }
 
-    public Empresa guardar_empresa(Empresa empresa) {
-        return empresaRepository.guardar_empresa(empresa);
+    public Empresa guardarEmpresa(Empresa empresa) {
+        return empresa_repository.guardarEmpresa(empresa);
     }
 
-    public String eliminarEmpresa(int id_emp) {
-        empresaRepository.eliminar_empresa(id_emp);
+    public String eliminarEmpresaPorId(int id_emp) {
+        empresa_repository.eliminarEmpresaPorId(id_emp);
         return "Empresa eliminada existosamente";
     }
 
     public Empresa actualizarEmpresa(Empresa empresa) {
-        return empresaRepository.actualizar_empresa(empresa);
+        return empresa_repository.actualizarEmpresa(empresa);
     }
 
 }
