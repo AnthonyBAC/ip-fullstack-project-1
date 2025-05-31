99db68e33e26ac758fe1f033efe6e5aea2f3bee9
Fabian <fa.verasay@duocuc.cl>
2025-05-31 13:32:09 -0400

    Se crea repository reporte general

--- microservicio-reporteGeneral/src/main/java/microservicio_reporteGeneral/microservicio_reporteGeneral/repository/ReporteGeneralRepository.java
+++ microservicio-reporteGeneral/src/main/java/microservicio_reporteGeneral/microservicio_reporteGeneral/repository/ReporteGeneralRepository.java
@@ -1,5 +1,13 @@
 package microservicio_reporteGeneral.microservicio_reporteGeneral.repository;
 
-public class ReporteGeneralRepository {
+import com.mitrack.reportegeneral.model.ReporteGeneralModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
+public interface ReporteGeneralRepository extends JpaRepository<ReporteGeneralModel, Long> {
+    
+    List<ReporteGeneralModel> findByTipoOperacion(String tipoOperacion);
+    List<ReporteGeneralModel> findByPatente(String patente);
 }
