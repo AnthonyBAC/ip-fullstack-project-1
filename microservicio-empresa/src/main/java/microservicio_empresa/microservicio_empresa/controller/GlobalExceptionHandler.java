54d5796fc12d2a14a056b50972759fdd1159b710
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-05-31 12:36:50 -0400

    modificacion Readme

--- microservicio-empresa/src/main/java/microservicio_empresa/microservicio_empresa/controller/GlobalExceptionHandler.java
+++ microservicio-empresa/src/main/java/microservicio_empresa/microservicio_empresa/controller/GlobalExceptionHandler.java
@@ -2,14 +2,21 @@ package microservicio_empresa.microservicio_empresa.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+// Excepsion global para todo controlador REST 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    // Excepcion global para manejar errores
+    // Si ocurre un exception usta este metodo
     @ExceptionHandler(RuntimeException.class)
+    // Metodo, recibe parametro, retorna respuesta HTTP con el mensaje
     public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
 }
+
+// 400 Bad Request
